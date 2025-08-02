16b038fa57d096de2244bb747a047aa244e9fc9c
Rodrigo Fúnes <rodrigofunes@hotmail.es>
2025-08-02 12:41:54 -0600

    get_pokemons & update_request

--- function_app.py
+++ function_app.py
@@ -1,39 +1,42 @@
 #import io
-#import os
+import os
 import azure.functions as func
 import datetime
 import json
 import logging
-#import requests
-#from dotenv import load_dotenv
+import requests
+from dotenv import load_dotenv
 #import pandas as pd
 #from azure.storage.blob import BlobServiceClient
 
 app = func.FunctionApp()
-#load_dotenv()
+load_dotenv()
 
 #logging.basicConfig( level=logging.INFO )
 #logger = logging.getLogger(__name__)
 
-#DOMAIN = os.getenv("DOMAIN")
+DOMAIN = os.getenv("DOMAIN")
 #AZURE_STORAGE_CONNECTION_STRING = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
 #BLOB_CONTAINER_NAME = os.getenv("BLOB_CONTAINER_NAME")
 #STORA_ACCOUNT_NAME = os.getenv("STORA_ACCOUNT_NAME")
 
 @app.queue_trigger(
-    arg_name="azqueue"
-    , queue_name="requests"
-    , connection="QueueAzureWebJobsStorage"
+    arg_name="azqueue", 
+    queue_name="requests", 
+    connection="QueueAzureWebJobsStorage"
 )
 def QueueTriggerPokeReport(azqueue: func.QueueMessage):
     body = azqueue.get_body().decode('utf-8')
     record = json.loads(body)
-    logging.info( f"message: { record }")
-#    id = record[0]["id"]
-#
-#    update_request( id , "inprogress" )
-#    request_info = get_request(id)
-#    pokemons = get_pokemons( request_info[0]["type"] )
+    id = record[0]["id"]
+
+    update_request( id , "inprogress" )
+
+    request_info = get_request(id)
+    pokemons = get_pokemons( request_info[0]["type"] )
+    logging.info( pokemons )
+    update_request( id , "completed" )
+
 #    pokemon_bytes = generate_csv_to_blob( pokemons )
 #    blob_name = f"poke_report_{id}.csv"
 #    upload_csv_to_blob( blob_name=blob_name, csv_data=pokemon_bytes )
@@ -42,28 +45,30 @@ def QueueTriggerPokeReport(azqueue: func.QueueMessage):
 #    url_completa = f"https://{STORA_ACCOUNT_NAME}.blob.core.windows.net/{BLOB_CONTAINER_NAME}/{blob_name}"
 #    update_request( id , "completed", url_completa )
 #
-#def update_request( id: int, status: str, url: str = None ) -> dict:
-#    payload = {
-#        "status": status
-#        , "id": id
-#    }
+def update_request( id: int, status: str, url: str = None ) -> dict:
+    payload = {
+        "status": status,
+        "id": id
+    }
 #    if url:
 #        payload["url"] = url
-#
-#    reponse = requests.put( f"{DOMAIN}/api/request" , json=payload )
-#    return reponse.json()
-#
-#def get_request(id: int) -> dict:
-#    reponse = requests.get( f"{DOMAIN}/api/request/{id}"  )
-#    return reponse.json()
-#
-#def get_pokemons( type: str ) -> dict:
-#    pokeapi_url = f"https://pokeapi.co/api/v2/type/{type}"
-#    response = requests.get(pokeapi_url, timeout=3000)
-#    data = response.json()
-#    pokemon_entries = data.get("pokemon", [] )
-#    return [ p["pokemon"] for p in pokemon_entries ]
-#
+
+    response = requests.put( f"{DOMAIN}/api/request" , json=payload )
+    return response.json()
+
+def get_request(id: int) -> dict:
+    response = requests.get( f"{DOMAIN}/api/request/{id}"  )
+    return response.json()
+
+def get_pokemons( type: str ) -> dict:
+    pokeapi_url = f"https://pokeapi.co/api/v2/type/{type}"
+
+    response = requests.get(pokeapi_url, timeout=3000)
+    data = response.json()
+    pokemon_entries = data.get('pokemon', [])
+
+    return [ p['pokemon'] for p in pokemon_entries ]
+
 #def generate_csv_to_blob( pokemon_list: list ) -> bytes:
 #    df = pd.DataFrame( pokemon_list )
 #    output = io.StringIO()
